7c2fa8e6fbaaf12d8ec205f745f3c7340499a1a6
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-05-10 23:16:13 +0330

    feat : CRUD api routes for products created

---
+++ src/app/api/product/[id]/route.ts
@@ -0,0 +1,66 @@
+import { NextRequest, NextResponse } from "next/server";
+const getAdminToken = (req: NextRequest) =>
+	req.cookies.get("access_token")?.value;
+
+// ADD
+
+export async function POST(req: NextRequest) {
+	const token = getAdminToken(req);
+	const body = await req.json();
+
+	const res = await fetch(`${process.env.BACKEND_URL}/api/products`, {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+		body: JSON.stringify(body),
+	});
+	const data = await res.json();
+	return NextResponse.json(data, { status: res.status });
+}
+
+// DELETE
+
+export async function DELETE(
+	req: NextRequest,
+	{ params }: { params: { id: string } }
+) {
+	const token = getAdminToken(req);
+
+	const res = await fetch(
+		`${process.env.BACKEND_URL}/api/products/${params.id}`,
+		{
+			method: "DELETE",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}
+	);
+
+	const data = await res.json();
+	return NextResponse.json(data, { status: res.status });
+}
+
+// UPDATE
+export async function PUT(
+	req: NextRequest,
+	{ params }: { params: { id: string } }
+) {
+	const token = getAdminToken(req);
+	const body = await req.json();
+
+	const res = await fetch(
+		`${process.env.BACKEND_URL}/api/products/${params.id}`,
+		{
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(body),
+		}
+	);
+
+	const data = await res.json();
+	return NextResponse.json(data, { status: res.status });
+}
